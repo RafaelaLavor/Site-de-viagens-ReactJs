c3835dc0780340ac967ee09a91565df3dbdeda14
Rafaela <adriainscrita@gmail.com>
2023-12-20 21:58:49 -0300

    a imagem voltou

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,13 +6,12 @@ import viao from "../../Assets/viao.jpg"
 const Home = () => {
     return (
         <section className="home">
-            <div className="overlay">
-                <img src={viao} alt="0" className="fundo" />
-                <div className="homeContent container">
-                    <div className="textDiv">
-                        <span className="smallText">Nossos Pacotes</span>
-                        <h1 className="homeTitle">Viaje Conosco</h1>
-                    </div>
+            <div className="overlay"></div>
+            <img src={viao} alt="0" className="fundo" />
+            <div className="homeContent container">
+                <div className="textDiv">
+                    <span className="smallText">Nossos Pacotes</span>
+                    <h1 className="homeTitle">Viaje Conosco</h1>
                 </div>
             </div>
         </section>
