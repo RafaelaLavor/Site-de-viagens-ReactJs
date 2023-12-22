4024e47946cb1cd32020777a136ba4b80c8ad739
Rafaela <adriainscrita@gmail.com>
2023-12-21 21:47:40 -0300

    corzinha rgb

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css"
 import viao from "../../Assets/viao.jpg"
+import { TbGps } from "react-icons/tb";
 
 
 const Home = () => {
@@ -13,8 +14,41 @@ const Home = () => {
                     <span className="smallText">Nossos Pacotes</span>
                     <h1 className="homeTitle">Viaje Conosco</h1>
                 </div>
+
+                <div className="cardDiv grid">
+                    <div className="destinetionInput">
+                        <label htmlFor="city">
+                            <p>Procure seu destino</p>
+                        </label>
+                        <div className="input flex">
+                            <input type="text" placeholder="Qual a cidade?" />
+                            <TbGps className="icon" />
+                        </div>
+                    </div>
+
+                    <div className="dateInput">
+                        <label htmlFor="date">
+                            <p>Datas</p>
+                        </label>
+                        <div className="input flex">
+                            <input type="date" />
+                        </div>
+                    </div>
+
+                    <div className="priceInput">
+                        <div className="labelTotal flex">
+                            <label htmlFor="date">
+                                Passagens até:
+                            </label>
+                            <h4 className="total">$10.000</h4>
+                        </div>
+                        <div className="input flex">
+                            <input type="range" max="10000" min="1000" />
+                        </div>
+                    </div>
+                </div>
             </div>
-        </section>
+        </section >
     )
 }
 
