ccb20b00e6c185fce6a1ef8ca1434e3aef8deaa1
Rafaela <adriainscrita@gmail.com>
2024-01-06 17:57:23 -0300

    Projeto completo em ReactJs

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,12 +1,16 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import "./Main.css"
 import img from "../../Assets/img.jpeg"
 import { GrMapLocation } from "react-icons/gr";
 import { FaClipboardList } from "react-icons/fa";
 import img2 from "../../Assets/img2.jpg"
+import aracaju from "../../Assets/aracaju.jpg"
+import porto from "../../Assets/porto.jpg"
+import manaus from "../../Assets/manaus.jpeg"
+import campo from "../../Assets/campo.jpg"
 
 import Aos from "aos";
-import "aos/dist/aos.css" 
+import "aos/dist/aos.css"
 
 const Data = [
     {
@@ -15,7 +19,7 @@ const Data = [
         destTitle: "Rio de janeiro",
         location: "Brasil, Rio de janeiro",
         grade: "FAUNA E FLORA",
-        fees: "$630",
+        fees: "$700",
         description: "O Rio de Janeiro, conhecido como a 'Cidade Maravilhosa', encanta com suas praias deslumbrantes, como Copacabana e Ipanema, e o icônico Cristo Redentor no Corcovado. A energia vibrante do carnaval e a vida noturna agitada refletem a rica cultura carioca."
     },
     {
@@ -27,7 +31,42 @@ const Data = [
         fees: "$830",
         description: "Natal, a cidade do sol no Rio Grande do Norte, encanta com suas praias de areias douradas, como Ponta Negra e Genipabu. Conhecida pela hospitalidade e cultura rica, Natal oferece uma experiência única, onde dunas, lagoas e festivais locais criam lembranças inesquecíveis à beira-mar."
     },
-
+    {
+        id: 3,
+        imgsrc: aracaju,
+        destTitle: "Aracaju",
+        location: "Brasil, Sergipe",
+        grade: "PRAIAS NORDESTINAS",
+        fees: "$720",
+        description: "Aracaju, capital sergipana, é uma cidade à beira-mar com um charme que mescla história e modernidade. Suas praias, como a famosa Atalaia, oferecem tranquilidade e belezas naturais. Com uma rica cultura culinária e um ambiente acolhedor, Aracaju é um destino encantador para quem busca explorar a diversidade do nordeste brasileiro."
+    },
+    {
+        id: 4,
+        imgsrc: manaus,
+        destTitle: "Manaus",
+        location: "Brasil, Amazonas",
+        grade: "NATUREZA E CULTURA",
+        fees: "$680",
+        description: "O Amazonas, gigante verde do Brasil, é lar da floresta amazônica, um tesouro de biodiversidade essencial para o equilíbrio global. O Rio Amazonas serpenteia a região, conectando ecossistemas únicos e comunidades que dependem dessa maravilha natural. A capital Manaus, entre o moderno e o selvagem, oferece uma porta de entrada para explorar a grandiosidade da Amazônia."
+    },
+    {
+        id: 5,
+        imgsrc: porto,
+        destTitle: "Campo grande",
+        location: "Brasil, Mato grosso do Sul",
+        grade: "CORAÇÃO DO PANTANAL",
+        fees: "$860",
+        description: "Campo Grande, capital de Mato Grosso do Sul, é uma cidade acolhedora que reflete a diversidade cultural e a riqueza natural do Pantanal. Conhecida pela hospitalidade, suas amplas avenidas arborizadas e parques proporcionam uma atmosfera tranquila, enquanto a proximidade com a maior planície alagável do mundo oferece oportunidades únicas de ecoturismo e preservação ambiental."
+    },
+    {
+        id: 6,
+        imgsrc: campo,
+        destTitle: "Porto Alegre",
+        location: "Brasil, Rio grande do Sul",
+        grade: "HARMONIA E TRADIÇÃO",
+        fees: "$920",
+        description: "Porto Alegre, a vibrante capital do Rio Grande do Sul, cativa visitantes com sua rica herança cultural e arquitetura marcante. Às margens do Rio Guaíba, a cidade oferece um cenário deslumbrante, com o pôr do sol no famoso pôr do sol do Gasômetro sendo um espetáculo imperdível. Conhecida por sua hospitalidade e diversidade gastronômica, Porto Alegre é uma fusão fascinante de tradição e modernidade no sul do Brasil."
+    },
 ]
 const Main = () => {
     useEffect(() => {
